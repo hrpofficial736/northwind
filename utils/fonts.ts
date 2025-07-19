5f388068d146545471b3cb9c6c898f2e75f67f78
Harshit Raj Pandey <hrpofficial736@gmail.com>
2025-07-19 13:00:32 +0530

    final commit

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,5 +1,6 @@
 import { Manrope } from "next/font/google";
 
 export const manropeFont = Manrope({
-    weight: ["300", "400", "600", "200", "500"]
+    weight: ["300", "400", "600", "200", "500"],
+    subsets: ["latin"]
 })
