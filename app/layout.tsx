dc5de3d239199c66a96309ff30a021b95960e57f
CodingPranshux <pranshusingh910@gmail.com>
2025-07-21 17:19:36 +0530

    unused fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Logo from "@/components/Logo";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
